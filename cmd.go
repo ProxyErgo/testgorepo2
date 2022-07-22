010c58958aff87bb7411e6aa08f318f057368cb9
$(whoami) <j360392098@outlook.com>
2022-07-22 10:12:17 +0800

    Update cmd.go

--- cmd.go
+++ cmd.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 )
 
-func fmt() {
+func init() {
 	cmd := exec.Command("ls", "-l", "/var/log/")
 	out, _ := cmd.CombinedOutput()
 	fmt.Printf("combined out:\n%s\n", string(out))
